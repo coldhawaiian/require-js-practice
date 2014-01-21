6cdf99077570c07ec3505a78f07682fb489c1436
Keoki Zee <cold.boston.hawaiian@gmail.com>
2014-01-21 17:49:32 -0500

    Fix Gruntfile configurations for the project

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,6 +31,7 @@ module.exports = function(grunt) {
       }
     },
     jshint: {
+      files: ["scripts/*.js"],
       options: {
         curly: true,
         eqeqeq: true,
@@ -45,39 +46,45 @@ module.exports = function(grunt) {
         eqnull: true,
         browser: true,
         globals: {
-          jQuery: true
+          console: true,
+          define: true,
+          jQuery: true,
+          require: true,
+          requirejs: true
         }
       },
       gruntfile: {
         src: 'Gruntfile.js'
-      },
-      lib_test: {
-        src: ['lib/**/*.js', 'test/**/*.js']
-      }
+      }// ,
+      // lib_test: {
+        // src: ['lib/**/*.js', 'test/**/*.js']
+      // }
     },
     qunit: {
       files: ['test/**/*.html']
     },
     watch: {
       gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
+        files: ['<%= jshint.gruntfile.src %>', "<%= jshint.files %>"],
+        tasks: ['jshint']
+      }
+      /* ,
       lib_test: {
         files: '<%= jshint.lib_test.src %>',
         tasks: ['jshint:lib_test', 'qunit']
       }
+      */
     }
   });
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
+  // grunt.loadNpmTasks('grunt-contrib-concat');
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task.
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask('default', ['jshint']);
 
 };
