3a326fe1df6c9a32e7b178d2352eae3148f1a6a4
Keoki Zee <cold.boston.hawaiian@gmail.com>
2014-01-22 00:40:24 -0500

    Wire up Fancy Input and Bootstrap starter template

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,19 @@
 requirejs.config({
-  baseUrl: "scripts"
+  baseUrl: "scripts",
+  paths: {
+    // "bootstrap": "../bower_components/bootstrap/dist/js",
+    "fancyinput": "../bower_components/fancyInput/fancyInput",
+    "jquery": "../bower_components/jquery/jquery"
+  },
+  shim: {
+    // "bootstrap": { deps: ["jquery"] },
+    "jquery.fancyinput": ["jquery"]
+  }
 });
 
-require(["player-factory"], function(playerFactory) {
+require(["jquery", "fancyinput", "player-factory"],
+function($, fancyinput, playerFactory) {
+  // Show the different player class stats
   var players = ["Rogue", "Warrior", "Wizard"].map(function(player) {
     return playerFactory.get(player);
   });
@@ -10,4 +21,7 @@ require(["player-factory"], function(playerFactory) {
   for (var i = 0, n = players.length; i < n; i++) {
     console.log(players[i].toString() + "\n");
   }
+
+  // Try out jQuery Fancy Input plugin
+  $("#cool-input").fancyInput();
 });
