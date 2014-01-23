6049e00f7d43c35398d8082d68e95cf853db765c
Keoki Zee <cold.boston.hawaiian@gmail.com>
2014-01-22 19:11:09 -0500

    Remove Fancy Input plugin and consolidate CSS

--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,14 @@ requirejs.config({
   baseUrl: "scripts",
   paths: {
     // "bootstrap": "../bower_components/bootstrap/dist/js",
-    "fancyinput": "../bower_components/fancyInput/fancyInput",
     "jquery": "../bower_components/jquery/jquery"
   },
   shim: {
     // "bootstrap": { deps: ["jquery"] },
-    "jquery.fancyinput": ["jquery"]
   }
 });
 
-require(["jquery", "fancyinput", "player-factory"],
-function($, fancyinput, playerFactory) {
+require(["jquery", "player-factory"], function($, playerFactory) {
   // Show the different player class stats
   var players = ["Rogue", "Warrior", "Wizard"].map(function(player) {
     return playerFactory.get(player);
@@ -21,7 +18,4 @@ function($, fancyinput, playerFactory) {
   for (var i = 0, n = players.length; i < n; i++) {
     console.log(players[i].toString() + "\n");
   }
-
-  // Try out jQuery Fancy Input plugin
-  $("#cool-input").fancyInput();
 });
